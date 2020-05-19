cbb806a023a6f573beaed5607b2979fd6e7c8eb1
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-05-19 15:25:51 +1000

    create db.query to insert the signup data from new user

--- trashure-server.js
+++ trashure-server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const db = require('./models/config')
 // const pg = require('pg')
 
 app.set('view engine', 'ejs')
@@ -13,6 +14,26 @@ app.get('/login', (req, res)=>{
 
 app.get('/signup', (req, res)=>{
     res.render('sign-up')
+})
+
+// insert the data when user sign-up
+app.post('/signup', (req, res)=>{
+
+    db.query(
+        'insert into trashure (username, name, email, password, avatar_url) values ($1, $2, $3, $4, $5)', [req.body.username, req.body.name, req.body.email, req.body.password, req.body.avatar_url], (err, dbRes)=>{
+
+            res.json({
+                username: req.body.username, 
+                name: req.body.name, 
+                email: req.body.email,
+                password: req.body.password,
+                avatar_url: req.body.avatar_url
+            })
+            
+            res.redirect('/')
+    })
+    
+
 })
 
 app.get('/myitems', (req,res)=>{
