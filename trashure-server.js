46a1402eca31f729c1e4113038f2f1be2e1b19a3
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-05-19 18:37:43 +1000

    create signup box and sign up post route

--- trashure-server.js
+++ trashure-server.js
@@ -3,6 +3,15 @@ const app = express()
 const port = 8080
 const db = require('./models/config')
 // const pg = require('pg')
+// installed bcrypt, express-session, passport-local, bodyParser
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+var session = require('express-session')
+
+var bodyParser = require('body-parser')
+
+// when you set it from the form  // .json from ajax(axios) request
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 
@@ -17,22 +26,23 @@ app.get('/signup', (req, res)=>{
 })
 
 // insert the data when user sign-up
+// encrypted password will be stored to db
 app.post('/signup', (req, res)=>{
 
+    const hash = bcrypt.hashSync(req.body.password, 10);
+
     db.query(
-        'insert into trashure (username, name, email, password, avatar_url) values ($1, $2, $3, $4, $5)', [req.body.username, req.body.name, req.body.email, req.body.password, req.body.avatar_url], (err, dbRes)=>{
+        'insert into users (username, name, email, password, avatar_url) values ($1, $2, $3, $4, $5)', [req.body.username, req.body.name, req.body.email, hash, req.body.avatar_url], (err, dbRes)=>{
 
             res.json({
                 username: req.body.username, 
                 name: req.body.name, 
                 email: req.body.email,
-                password: req.body.password,
                 avatar_url: req.body.avatar_url
             })
             
             res.redirect('/')
     })
-    
 
 })
 
