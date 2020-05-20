b944748c5e552712b6eff7d0d7e170d4b0ebf799
debbieritapaterson <deborahritapaterson@gmail.com>
2020-05-20 11:28:28 +1000

    user-location from browser

--- models/user-location.js
+++ models/user-location.js
@@ -1,34 +1,42 @@
-//var x = document.getElementById("demo");
-
-let userLocation = {};
+let userLocation = {"latitude": -38.814629, "longitude": 144.963384};
 
 function getLocation() {
   if (navigator.geolocation) {
-      //console.log(navigator.geolocation)
+      console.log(userLocation)
       navigator.geolocation.getCurrentPosition(showPosition);
-  } 
-//   else {
-//     //x.innerHTML = "Geolocation is not supported by this browser.";
-//   }
+
+
+  } else {
+        //For ERROR Handling
+        // If user or browser blocks location sharing, then default userLocation is center of Melboure
+        userLocation = {"latitude": -37.814629, "longitude": 144.963384}
+    }
 }
 
 function showPosition(position) {
-    console.log(position)
-    userLocation = {"latitude": position.coords.latitude,
-  "longitude": position.coords.longitude}
-  console.log(userLocation)
-  return userLocation
+    //console.log(position)
+
+    userLocation = {
+        "latitude": position.coords.latitude, 
+        "longitude": position.coords.longitude
+    }
+    //console.log(userLocation)
+    
 }
 
-let loc = getLocation()
-// console.log(location)
+module.exports = {
+    getLocation: getLocation(),
+    userLocation: userLocation
+}
 
 
-// module.exports = {
-//     location: getLocation,
+// For client.js file
 
-// }
+//const userLocation = require('./public/user-location.js')
 
-//center: new Microsoft.Maps.Location(51.50632, -0.12714)
+// replace 'center: new Microsoft.Maps.Location(51.50632, -0.12714)' with below
 
-//center: new Microsoft.Maps.Location(location.latitude, location.longitude)
+//center: new Microsoft.Maps.Location(userLocation.latitude, userLocation.longitude)
+
+// //For HTML File
+//<body onload="getLocation()"></body>
