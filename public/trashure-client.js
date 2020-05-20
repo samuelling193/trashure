ff9a05cb2f7ab52893c2bb3c1537e04207979cbe
debbieritapaterson <deborahritapaterson@gmail.com>
2020-05-20 21:34:58 +1000

    nav bar skeleton and map centering from browser location

--- public/trashure-client.js
+++ public/trashure-client.js
@@ -1,5 +1,32 @@
 console.log("trashure client js successfully connected")
 
+//gets user location from browser
+
+function getLocation(cb) {
+  if (navigator.geolocation) {
+      //console.log(userLocation)
+      navigator.geolocation.getCurrentPosition((position) => showPosition(position, cb));
+
+
+  } else {
+        //For ERROR Handling
+        // If user or browser blocks location sharing, then default userLocation is center of Melboure
+        var userLocation = {"latitude": -37.814629, "longitude": 144.963384}
+
+        cb(userLocation)
+    }
+}
+
+function showPosition(position, cb) {
+
+    var userLocation = {
+        "latitude": position.coords.latitude, 
+        "longitude": position.coords.longitude
+    }
+
+    cb(userLocation)
+    //console.log(userLocation) 
+}
 
 // Fetches information to be displayed
 const handlePinClick = function (e) {
@@ -23,54 +50,63 @@ const handlePinClick = function (e) {
 }
 
 function getMap() {
-    var map = new Microsoft.Maps.Map('#map', {
-                                            // user location 
-        center: new Microsoft.Maps.Location(-37.7989538, 144.9597395)
-    });
 
-    var center = map.getCenter()
+    getLocation(function (userLocation){
 
-    const url = 'http://localhost:8080/api/trashure_items'
+        var map = new Microsoft.Maps.Map('#map', {
+                                                // user location 
+            center: new Microsoft.Maps.Location(userLocation.latitude, userLocation.longitude),
+            zoom: 10
+        });
 
-    axios.get(url).then(res => {
-        res.data.forEach(function(data) {
-            var location = { latitude: data.lat, longitude: data.long}
-            pin = new Microsoft.Maps.Pushpin(location, {
-                title: data.name,
-                text: `${data.id}`
+        var center = map.getCenter()
+        console.log(center)
+
+        const url = 'http://localhost:8080/api/trashure_items'
+
+        axios.get(url).then(res => {
+            res.data.forEach(function(data) {
+                var location = { latitude: data.lat, longitude: data.long}
+                pin = new Microsoft.Maps.Pushpin(location, {
+                    title: data.name,
+                    text: `${data.id}`
+                })
+                map.entities.push(pin)
+                
+                // Handles the click event of a pin, note changes textContent of Div to display it
+                Microsoft.Maps.Events.addHandler(pin, 'click', handlePinClick)
             })
-            map.entities.push(pin)
-            
-            // Handles the click event of a pin, note changes textContent of Div to display it
-            Microsoft.Maps.Events.addHandler(pin, 'click', handlePinClick)
+        })
+        
+
+        // Create custom Pushpin
+        // var pin  = new Microsoft.Maps.Pushpin(center, {
+        //     title: 'Melbourne',
+        //     subTitle: 'City Center',
+        //     text: '1'
+        // })
+
+        // Fetches information to be displayed
+        const getInformationAboutItem = (e) => { 
+            // Needs the right information
+            return(e.target._options.itemId) 
+        }
+
+        axios.get(url).then(res => {
+            res.data.forEach(function(data) {
+                var location = { 
+                    latitude: data.lat, 
+                    longitude: data.long
+                }
+                pin = new Microsoft.Maps.Pushpin(location, {
+                    title: data.name,
+                    text: `${data.id}`
+                })
+                map.entities.push(pin)
+                
+                // Handles the click event of a pin, note changes textContent of Div to display it
+                Microsoft.Maps.Events.addHandler(pin, 'click', handlePinClick)
+            })
         })
     })
-    
-
-    // Create custom Pushpin
-    // var pin  = new Microsoft.Maps.Pushpin(center, {
-    //     title: 'Melbourne',
-    //     subTitle: 'City Center',
-    //     text: '1'
-    // })
-
-    // Fetches information to be displayed
-    const getInformationAboutItem = (e) => { 
-        // Needs the right information
-        return(e.target._options.itemId) 
-    }
-
-    axios.get(url).then(res => {
-        res.data.forEach(function(data) {
-            var location = { latitude: data.lat, longitude: data.long}
-            pin = new Microsoft.Maps.Pushpin(location, {
-                title: data.name,
-                text: `${data.id}`
-            })
-            map.entities.push(pin)
-            
-            // Handles the click event of a pin, note changes textContent of Div to display it
-            Microsoft.Maps.Events.addHandler(pin, 'click', handlePinClick)
-            })
-        })
 }
